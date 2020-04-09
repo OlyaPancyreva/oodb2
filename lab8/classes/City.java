09451ea3d2ef6801985e5026ce7acfcd56e8e20d
O'Lik <55744134+OlyaPancyreva@users.noreply.github.com>
2020-04-09 13:54:41 +0300

    Create City.java

---
+++ lab8/classes/City.java
@@ -0,0 +1,19 @@
+package Lab8.classes;
+
+import Lab6.annotation.Entity;
+import Lab7.annotation.Column;
+import Lab7.annotation.Id;
+import Lab7.annotation.ManyToOne;
+
+@Entity
+public class City{
+
+    @Id
+    Long id;
+    @Column
+    String city;
+    @Column
+    String address;
+    @Column
+    String hours;
+}
